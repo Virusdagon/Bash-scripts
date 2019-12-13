c99bd855c91ba0ecafe9c466f32d3f63d0ea536f
Muzychenko Oleksii <virusdagon@gmail.com>
2019-12-13 08:52:26 +0200

    0.7.6
    Regular update

--- security_check.sh
+++ security_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Version 0.7.5
+#Version 0.7.6
 #Oleksii Muzychenko and Andrew Prokofiev
 
 user="$1"
@@ -37,7 +37,7 @@ if [ ! -d "/home/$user/public_html" ]
 					echo -n "Current Old Drupal version are 5.23, 6.38 and: "
                			 	echo "${drupal7}"
 		                	for i in 226 227 228 229 230 231; do echo -en "\e[38;5;${i}m=================\e[0m"; done; echo
-		                	find -type f -iwholename "*/modules/system/system.info" -exec grep -H "version = \"" {} \;|grep -v "7.67"|grep -v "5.23" |grep -v "6.38"
+		                	find -type f -iwholename "*/modules/system/system.info" -exec grep -H "version = \"" {} \;|grep -v "7.68"|grep -v "5.23" |grep -v "6.38"
 		                	for i in 226 227 228 229 230 231; do echo -en "\e[38;5;${i}m=================\e[0m"; done; echo
 	fi
 
@@ -48,7 +48,7 @@ if [ ! -d "/home/$user/public_html" ]
                                         echo -n "Current Drupal 8 version is: "
                                         echo "${drupal8}"
                                         for i in 196 160 124 88 52 16; do echo -en "\e[38;5;${i}m=================\e[0m"; done; echo
-                                        find -type f -iwholename "*/core/lib/Drupal.php" -exec grep -H "const VERSION =" {} \;|grep -v "8.7.10"
+                                        find -type f -iwholename "*/core/lib/Drupal.php" -exec grep -H "const VERSION =" {} \;|grep -v "8.8.0"
                                         for i in 196 160 124 88 52 16; do echo -en "\e[38;5;${i}m=================\e[0m"; done; echo
         fi
 
@@ -68,7 +68,7 @@ if [ ! -d "/home/$user/public_html" ]
 					echo -n "Current WordPress version is: "
 					echo "${wordpress}"
 		                	for i in 52 53 54 55 56 57; do echo -en "\e[38;5;${i}m=================\e[0m"; done; echo
-		                	find . -type f -iwholename "*/wp-includes/version.php" -exec grep -H "\$wp_version =" {} \;|grep -v "5.3"
+		                	find . -type f -iwholename "*/wp-includes/version.php" -exec grep -H "\$wp_version =" {} \;|grep -v "5.3.1"
 		                	for i in 52 53 54 55 56 57; do echo -en "\e[38;5;${i}m=================\e[0m"; done; echo
 
                         echo -e '\033[1m'		
